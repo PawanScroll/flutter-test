9825f93b9a2dfc5f2428d9b2d20829932b524b15
Pawan <pawan@scrollstack.com>
2024-07-23 02:37:42 +0530

    palette generator gradient added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:provider/provider.dart';
+import 'package:palette_generator/palette_generator.dart';
 
 import 'package:stck_site/store/featured_posts.dart';
 import 'package:stck_site/models/post.dart';
@@ -15,6 +17,11 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          context.push('/search');
+        },
+      ),
       body: ChangeNotifierProvider(
           create: (context) => FeaturedPosts(), child: const ListContainer()),
     );
@@ -40,27 +47,43 @@ class _ListContainerState extends State<ListContainer> {
   @override
   Widget build(BuildContext context) {
     return Consumer<FeaturedPosts>(builder: (context, featuredPosts, child) {
-      return ListView.builder(
-          itemCount: featuredPosts.posts.length,
-          itemBuilder: (context, index) {
-            return ListTile(post: featuredPosts.posts[index]);
-          });
+      return PageView.builder(
+        scrollDirection: Axis.vertical,
+        itemCount: featuredPosts.posts.length,
+        itemBuilder: (context, index) {
+          return PostTile(post: featuredPosts.posts[index]);
+        },
+      );
     });
   }
 }
 
-class ListTile extends StatefulWidget {
+class PostTile extends StatefulWidget {
   final Post post;
-  const ListTile({super.key, required this.post});
+  const PostTile({super.key, required this.post});
 
   @override
-  State<ListTile> createState() => _ListTileState();
+  State<PostTile> createState() => _PostTileState();
 }
 
-class _ListTileState extends State<ListTile>
+class _PostTileState extends State<PostTile>
     with SingleTickerProviderStateMixin {
   late AnimationController _controller;
   late Animation<Offset> _animation;
+
+  PaletteGenerator? paletteGenerator;
+
+  ImageSrc imageSrc = const ImageSrc();
+
+  Future<void> _updatePaletteGenerator() async {
+    paletteGenerator = await PaletteGenerator.fromImageProvider(
+      NetworkImage(imageSrc.fallback ?? ''),
+      size: Size(imageSrc.meta?.height.toDouble() ?? 0,
+          imageSrc.meta?.width.toDouble() ?? 0),
+      maximumColorCount: 20,
+    );
+    setState(() {});
+  }
 
   @override
   void initState() {
@@ -73,6 +96,11 @@ class _ListTileState extends State<ListTile>
       begin: const Offset(0, 0),
       end: const Offset(0, 0.05),
     ).animate(_controller);
+
+    if (widget.post.meta.cover.src is ImageSrc) {
+      imageSrc = widget.post.meta.cover.src as ImageSrc;
+    }
+    _updatePaletteGenerator();
   }
 
   @override
@@ -83,57 +111,61 @@ class _ListTileState extends State<ListTile>
 
   @override
   Widget build(BuildContext context) {
-    ImageSrc imageSrc = const ImageSrc();
-    if (widget.post.meta.cover.src is ImageSrc) {
-      imageSrc = widget.post.meta.cover.src as ImageSrc;
-    }
     return Container(
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height,
       padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
         border: Border.all(color: Colors.grey),
+        gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              paletteGenerator?.lightVibrantColor?.color ?? Colors.grey,
+              paletteGenerator?.lightMutedColor?.color ?? Colors.grey,
+            ]),
       ),
-      child: Stack(
+      child: Column(
         children: [
-          const Positioned(
-            top: 10,
-            child: Center(
-              child: Text(
-                'stck.me',
-                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+          const Text(
+            'stck.me',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 36),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: SlideTransition(
+              position: _animation,
+              child: Image.network(
+                imageSrc.fallback ?? '',
+                height: 200,
               ),
             ),
           ),
-          Column(
-            children: [
-              SlideTransition(
-                position: _animation,
-                child: Image.network(
-                  imageSrc.fallback ?? '',
-                  height: 200,
-                ),
-              ),
-              const SizedBox(height: 10),
-              Text(
-                widget.post.author.name,
-                style: const TextStyle(fontWeight: FontWeight.bold),
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 10),
-              Text(
-                widget.post.title,
-                textAlign: TextAlign.center,
-              ),
-              const SizedBox(height: 10),
-              Text('PRICE \$${widget.post.pricing?['USD']}'),
-              const SizedBox(height: 10),
-              ElevatedButton(
-                onPressed: () {},
-                child: const Text('Buy'),
-              ),
-            ],
+          const SizedBox(height: 20),
+          Text(
+            widget.post.title,
+            style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 32),
+            textAlign: TextAlign.center,
+            maxLines: 2,
+            overflow: TextOverflow.ellipsis,
           ),
+          const SizedBox(height: 10),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage(
+                widget.post.author.avatar?.fallback ?? '',
+              ),
+            ),
+            title: Text(widget.post.author.name),
+          ),
+          const SizedBox(height: 10),
+          Text(
+            widget.post.summary,
+            overflow: TextOverflow.ellipsis,
+            maxLines: 2,
+          ),
+          const SizedBox(height: 10),
+          Text('PRICE ₹ ${widget.post.pricing?['INR']}'),
         ],
       ),
     );
